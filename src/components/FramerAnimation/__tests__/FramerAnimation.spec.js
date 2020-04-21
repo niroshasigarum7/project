93269fc7046e94aaef5d7f91a489af47de081eb5
Nirosha Sigarum <nirosha.sj25@gmail.com>
2020-04-21 07:15:21 +0530

    Task 15: Modifying unit test cases for FramerAnimation component

---
+++ src/components/FramerAnimation/__tests__/FramerAnimation.spec.js
@@ -0,0 +1,8 @@
+import React from 'react';
+    import renderer from 'react-test-renderer';
+    
+    describe("The FramerAnimation", () => {
+        it ("renders as expected", () => {
+            console.log("I am cutomizing it");
+        })
+    })
