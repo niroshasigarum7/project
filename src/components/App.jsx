6b32a799a5fb4abb598e635a44c21ef306e2e046
Nirosha Sigarum <nirosha.sj25@gmail.com>
2020-04-25 18:10:43 +0530

    Task 21: Added React - router and tabs container to the application

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,27 +15,36 @@ import Button from './Button/Button'
 import NewsItem from './NewsItem/NewsItem'
 import Loading from './Loading/Loading'
 
-
+import { Route, Switch } from 'react-router-dom'
 const App = () => (
-    <div id="app">
-        {/* <FramerAnimation /> */}
-        <div className="header__title"><h3>Globetrotters</h3></div>
-        <div id="add-todo">
-            <AddTodo />
+    <Switch>
+        <div id="app">
+            <div className="header__title"><h3>Globetrotters</h3></div>
+            <div className="tabs-container">
+                <a href="/addtodo">Add todo</a>
+                <a href="/slider">Slider App</a>
+                <a href="/notification">Notification</a>
+                <a href="/loading">loading</a>
+                <a href="/news-item">news-item</a>
+                <a href="/paint">paint</a>
+                <a href="/framer-animation">framer-animation</a>
+                <a href="/unit-testing">unit-testing</a>
+                <a href="/counter">counter</a>
+            </div>
+            <Route path='/addtodo' component={AddTodo} />
+            <Route path='/addtodo' component={Footer} />
+            <Route path='/addtodo' component={VisibleTodoList} />
+            <Route path='/slider' component={SliderApp} />
+            <Route path='/notification' component={Notification} />
+            <Route path='/loading' component={Loading} />
+            <Route path='/button' component={Button} />
+            <Route path='/news-item' component={NewsItem} />
+            <Route path='/paint' component={Paint} />
+            <Route path='/framer-animation' component={FramerAnimation} />
+            <Route path='/unit-testing' component={UnitTesting} />
+            <Route path='/counter' component={Counter} />
         </div>
-        <div id="footer-todo">
-            <Footer />
-        </div>
-        <div id="visible-todo">
-            <h2>Testing keyframe animation</h2> 
-            <VisibleTodoList />
-        </div>
-        {/* <Notification />
-        {/* <UnitTesting /> */}
-        {/* <div className="slider-wrapper">
-            <SliderApp />
-        </div> */}
-    </div>
+    </Switch> 
 )
 
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App.jsx'
-import './index.css';
+import './index.css'
+import createSagaMiddleware from 'redux-saga'
+import { helloSaga } from './sagas/Counter/couter-saga'
+import { BrowserRouter } from 'react-router-dom'
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(
+    rootReducer,
+    applyMiddleware(sagaMiddleware)
+)
 
-const store = createStore(rootReducer)
+sagaMiddleware.run(helloSaga)
 
 render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>,
     document.getElementById('root')
 )
